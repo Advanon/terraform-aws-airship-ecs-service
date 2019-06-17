b5563c321586cd7dfdea15e946d6774afe0bffde
bsAdvanon <bogdan.suvar@advanon.com>
2019-06-17 15:14:13 +0200

    fmt

--- main.tf
+++ main.tf
@@ -263,30 +263,29 @@ module "ecs_autoscaling" {
   scaling_properties = "${var.scaling_properties}"
 }
 
-
 resource "aws_security_group" "ecs_service_sg" {
-  vpc_id = "${data.aws_vpc.this.id}"
+  vpc_id      = "${data.aws_vpc.this.id}"
   description = "${var.name} ECS service tasks security group"
-  name = "${var.name}-ecs-service"
+  name        = "${var.name}-ecs-service"
 
   ingress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["${data.aws_vpc.this.cidr_block}"]
   }
 
   egress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 data "aws_vpc" "this" {
   filter {
-    name = "tag:Name"
+    name   = "tag:Name"
     values = ["${var.vpc_name}"]
   }
 }

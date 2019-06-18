3f025bc2d647e0855d2be8e0f16f780cd8d9eaa9
bsAdvanon <bogdan.suvar@advanon.com>
2019-06-18 15:36:45 +0200

    fix json output

--- scripts/get-ssm-param-names.sh
+++ scripts/get-ssm-param-names.sh
@@ -3,12 +3,22 @@
 
 function check_deps() {
   test -f $(which aws) || error_exit "aws not found, please install it"
-  test -f $(which jq) || error_exit "jq not found in path, please install it"
+  test -f $(which jq) || error_exit "jq not found, please install it"
 }
 
 function parse_input() {
   eval "$(jq -r '@sh "REGION=\(.region) SSM_PATH=\(.ssm_path)"')"
 }
 
+function generate_output(){
+  aws ssm get-parameters-by-path --path $SSM_PATH --region $REGION | \
+  jq '.[][].Name' | \
+  awk '{split($0,a,"/"); print a[4]}' | \
+  sed -e 's/^/\"/' | \
+  jq -s . | \
+  jq '{ vars: .}'
+}
+
 check_deps
-aws ssm get-parameters-by-path --path $SSM_PATH --region $REGION | jq '.[][].Name' | jq -s .
+parse_input
+generate_output
